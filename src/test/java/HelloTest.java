6899c81359c8ea822ddf7e0ebcfd6a7b1d0f802b
Mevlut Doger <mdoger@hotmail.com>
2022-12-26 18:52:43 +0000

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello Test");
+        System.out.println("about to share project on GitHub");
     }
 }
